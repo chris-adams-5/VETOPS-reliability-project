5b934741ec72a6404a5b9f11dc91e525c8dcb14d
Ian Hay <techsavhay@gmail.com>
2026-07-14 10:33:42 +0100

    commented out ip filtering and changed to 10% live traffic to cache

--- Terraform/alb_routing.tf
+++ Terraform/alb_routing.tf
@@ -26,14 +26,14 @@ resource "aws_lb_listener_rule" "canary_cache_hospital_route" {
       # THIS IS THE PERCENTAGE TRAFFIC TO BE SENT THE LAMBDA CACHE
       target_group {
         arn    = aws_lb_target_group.lambda_cache_tg.arn
-        weight = 100
+        weight = 90
       }
 
       # THIS IS THE TRAFFIC GOING TO THE original vendor DB
       target_group {
         # existing vendor target group ARN
         arn    = "arn:aws:elasticloadbalancing:eu-west-2:664047078509:targetgroup/lb-tg-VetOp/7e8634d3230cb907"
-        weight = 0
+        weight = 10
       }
     }
   }
@@ -55,10 +55,10 @@ resource "aws_lb_listener_rule" "canary_cache_hospital_route" {
 
   # optional IP filtering
   # uncomment to restrict the test to just these IPs
-  condition {
-    source_ip {
-      values = ["54.86.50.139/32", "86.19.92.50/32", "46.208.96.91/32"] # add tester(s) IP ADDRESSES HERE!
-    }
-  }
-}
+#   condition {
+#     source_ip {
+#       values = ["54.86.50.139/32", "86.19.92.50/32", "46.208.96.91/32"] # add tester(s) IP ADDRESSES HERE!
+#     }
+#   }
+# }
 #"46.208.96.91/32",
